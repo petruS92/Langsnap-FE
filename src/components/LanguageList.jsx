7b746d1dad1bbfcef94c276dd56e9b91f58c6c08
Petru <petru@Petrus-MacBook.local>
2020-06-11 09:18:09 +0100

    finished writing the LanguageList to filter for German words and display words from different languages

--- src/components/LanguageList.jsx
+++ src/components/LanguageList.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import * as wordsFunctions from "../utils/wordsListFunctions";
+
 const LanguageList = (props) => {
   const { selectedDisplayLanguage, selectedDisplayWords } = props;
+
+  const germanWordsFilter = (word) => {
+    if (selectedDisplayLanguage === "German") {
+      return wordsFunctions.capitaliseGermanWord(word);
+    } else {
+      return word;
+    }
+  };
+
   return (
     <div>
       <h4>{selectedDisplayLanguage}</h4>
+      <ul>
+        {selectedDisplayWords.map((wordObject, index) => {
+          return (
+            <li key={index}>{`${Object.keys(wordObject)} - ${germanWordsFilter(
+              Object.values(wordObject)
+            )}`}</li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
