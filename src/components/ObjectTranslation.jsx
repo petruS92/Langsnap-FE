8eccf33ecf3527626366076416d75ffecfd27772
tim0git <tim.doolan88@icloud.com>
2020-06-17 14:27:30 +0100

    remove default styling from translate menu

--- src/components/ObjectTranslation.jsx
+++ src/components/ObjectTranslation.jsx
@@ -127,7 +127,7 @@ class ObjectTranslation extends Component {
           ) : (
             <>
               <div className="translateButtonContainer">
-                <DropDown changeLanguage={this.changeLanguage} />
+                <DropDown changeLanguage={this.changeLanguage} className="translateDropDown"/>
                 <button
                   onClick={this.handleClickTranslate}
                   className="capture"
