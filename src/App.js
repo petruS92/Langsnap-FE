0d901bdf1460ed7840a7324ed66b86c3cef23f7c
Petru <petru@Petrus-MacBook.local>
2020-06-18 11:51:17 +0100

    Refactored App.js syntax and destructuring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-import ObjectTranslation from "./components/ObjectTranslation";
 import { Router } from "@reach/router";
 import "./App.css";
+import ObjectTranslation from "./components/ObjectTranslation";
 import SignUp from "./components/SignUp";
 import NavBar from "./components/NavBar";
 import setAuthToken from "./utils/authentication";
 import Login from "./components/Login";
 import MyAccount from "./components/MyAccounts";
-import * as wordsFunctions from "./utils/wordsListFunctions";
 import WordsList from "./components/WordsList";
 import Loading from "./components/Loading";
 import Game from "./components/Game/Game";
 import ErrorDisplay from "./components/ErrorDisplay";
+import * as wordsFunctions from "./utils/wordsListFunctions";
 
 class App extends Component {
   state = {
@@ -24,22 +24,70 @@ class App extends Component {
     isLoading: true,
   };
 
+  render() {
+    const {
+      isLoggedIn,
+      words,
+      email,
+      name,
+      token,
+      isLoggedOut,
+      isLoading,
+    } = this.state;
+    const { loggingIn, loggingOut, addNewWordToState } = this;
+
+    if (isLoading) return <Loading />;
+
+    return (
+      <section className="appHomeContainer">
+        <NavBar isLoggedIn={isLoggedIn} loggingOut={loggingOut} />
+
+        <Router>
+          <ObjectTranslation
+            path="/"
+            addNewWordToState={addNewWordToState}
+            isLoggedIn={isLoggedIn}
+            token={token}
+            words={words}
+          />
+          <SignUp path="/signup" loggingIn={loggingIn} />
+          <Login
+            path="/login"
+            isLoggedIn={isLoggedIn}
+            isLoggedOut={isLoggedOut}
+            loggingIn={loggingIn}
+          />
+          <MyAccount
+            path="/myaccount"
+            email={email}
+            name={name}
+            token={token}
+            words={words}
+          />
+          <WordsList path="/wordslist" words={words} isLoggedIn={isLoggedIn} />
+          <Game
+            path="/game"
+            words={words}
+            name={name}
+            isLoggedIn={isLoggedIn}
+          />
+          <ErrorDisplay isLoggedIn={isLoggedIn} default />
+        </Router>
+      </section>
+    );
+  }
+
   componentDidMount() {
     this.setState({ isLoading: false });
   }
 
   componentDidUpdate(previousProps, previousState) {
     const { token } = this.state;
-    const tokenReceived = this.state.token !== previousState.token;
+    const tokenReceived = token !== previousState.token;
     if (tokenReceived) {
       setAuthToken(token);
     }
   }
-
-  addNewWordToState = (updatedWords) => {
-    const arrayUpdatedWords = wordsFunctions.selectWords(updatedWords);
-    this.setState({ words: arrayUpdatedWords });
-  };
 
   loggingIn = (info) => {
     const {
@@ -67,55 +115,10 @@ class App extends Component {
     });
   };
 
-  render() {
-    const {
-      isLoggedIn,
-      words,
-      email,
-      name,
-      token,
-      isLoggedOut,
-      isLoading,
-    } = this.state;
-    if (isLoading) return <Loading />;
-    return (
-      <section className="appHomeContainer">
-        <NavBar isLoggedIn={isLoggedIn} loggingOut={this.loggingOut} />
-
-        <Router>
-          <ObjectTranslation
-            path="/"
-            addNewWordToState={this.addNewWordToState}
-            isLoggedIn={isLoggedIn}
-            token={token}
-            words={words}
-          />
-          <SignUp path="/signup" loggingIn={this.loggingIn} />
-          <Login
-            path="/login"
-            isLoggedIn={isLoggedIn}
-            isLoggedOut={isLoggedOut}
-            loggingIn={this.loggingIn}
-          />
-          <MyAccount
-            path="/myaccount"
-            email={email}
-            name={name}
-            token={token}
-            words={words}
-          />
-          <WordsList path="/wordslist" words={words} isLoggedIn={isLoggedIn} />
-          <Game
-            path="/game"
-            words={words}
-            name={name}
-            isLoggedIn={isLoggedIn}
-          />
-          <ErrorDisplay isLoggedIn={isLoggedIn} default />
-        </Router>
-      </section>
-    );
-  }
+  addNewWordToState = (updatedWords) => {
+    const arrayUpdatedWords = wordsFunctions.selectWords(updatedWords);
+    this.setState({ words: arrayUpdatedWords });
+  };
 }
 
 export default App;
