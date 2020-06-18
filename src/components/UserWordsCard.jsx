adaaec7af2059dd58c31cf8033ca8d135931113c
Petru <petru@Petrus-MacBook.local>
2020-06-18 16:06:55 +0100

    UserWordsCard refactored for better syntax and destructuring

--- src/components/UserWordsCard.jsx
+++ src/components/UserWordsCard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Loading from "./Loading";
-
 import LanguageList from "./LanguageList";
 import LoginAlert from "./LoginAlert";
 
@@ -11,8 +10,60 @@ class UserWordsCard extends React.Component {
     isLoading: true,
   };
 
+  render() {
+    const { isLoggedIn } = this.props;
+    const { selectedDisplayWords, isLoading } = this.state;
+    const { handleLanguageChange } = this;
+
+    if (!isLoggedIn) return <LoginAlert />;
+
+    if (isLoading) return <Loading />;
+
+    return (
+      <main className="pageContainer">
+        <div className="titleBackground">
+          <div className="titleContainer">
+            <h1 className="titleHeader">Words</h1>
+          </div>
+        </div>
+        <div className="contentBackground">
+          <section className="contentContainer">
+            <div className="wordsLabelContainer">
+              <select
+                onChange={handleLanguageChange}
+                className="wordsLabel"
+                default={"German"}
+              >
+                <option default value="German">
+                  German
+                </option>
+                <option value="French">French</option>
+                <option value="Spanish">Spanish</option>
+              </select>
+              <span
+                role="img"
+                aria-label="select-arrow"
+                className="dropDownArrowWordList"
+              >
+                ▼
+              </span>
+            </div>
+            {selectedDisplayWords.length > 0 ? (
+              <LanguageList selectedDisplayWords={selectedDisplayWords} />
+            ) : (
+              <p className="wordsListMessage">
+                Please translate words to view your words.
+              </p>
+            )}
+          </section>
+        </div>
+      </main>
+    );
+  }
+
   componentDidMount() {
     const { isLoggedIn, words } = this.props;
+
     if (isLoggedIn && words) {
       this.setState({
         selectedDisplayLanguage: "German",
@@ -26,13 +77,10 @@ class UserWordsCard extends React.Component {
     }
   }
 
-  handleLanguageChange = (event) => {
-    this.setState({ isLoading: true });
-    const {
-      target: { value },
-    } = event;
-
+  handleLanguageChange = ({ target: { value } }) => {
     const { words } = this.props;
+    this.setState({ isLoading: true });
+
     if (words !== null) {
       if (value === "German" && words.German) {
         this.setState({
@@ -67,54 +115,6 @@ class UserWordsCard extends React.Component {
       });
     }
   };
-
-  render() {
-    const { isLoggedIn } = this.props;
-    const { selectedDisplayWords, isLoading } = this.state;
-    if (!isLoggedIn) return <LoginAlert />;
-    if (isLoading) return <Loading />;
-    return (
-      <main className="pageContainer">
-        <div className="titleBackground">
-          <div className="titleContainer">
-            <h1 className="titleHeader">Words</h1>
-          </div>
-        </div>
-        <div className="contentBackground">
-          <section className="contentContainer">
-            <div className="wordsLabelContainer">
-              <select
-                onChange={this.handleLanguageChange}
-                className="wordsLabel"
-                default={"German"}
-              >
-                {/* <option>Choose language ...</option> */}
-                <option default value="German">
-                  German
-                </option>
-                <option value="French">French</option>
-                <option value="Spanish">Spanish</option>
-              </select>
-              <span
-                role="img"
-                aria-label="select-arrow"
-                className="dropDownArrowWordList"
-              >
-                ▼
-              </span>
-            </div>
-            {selectedDisplayWords.length > 0 ? (
-              <LanguageList selectedDisplayWords={selectedDisplayWords} />
-            ) : (
-              <p className="wordsListMessage">
-                Please translate words to view your words.
-              </p>
-            )}
-          </section>
-        </div>
-      </main>
-    );
-  }
 }
 
 export default UserWordsCard;
