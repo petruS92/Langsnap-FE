8255fa4a86d1f5bc9e33a76176fa207b4316a835
tim0git <tim.doolan88@icloud.com>
2020-06-18 10:59:09 +0100

    css now added from purge Branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React, { Component } from "react";
 import ObjectTranslation from "./components/ObjectTranslation";
 import { Router } from "@reach/router";
-import "./App.css";
-import "./StyleSheets/Login.css";
-import "./StyleSheets/NavBar.css";
-import "./StyleSheets/Game.css";
-import "./StyleSheets/LanguageList.css";
-import "./StyleSheets/MyAccounts.css";
-import "./StyleSheets/AssociatedWords.css";
+import "./StyleSheets/App.css";
 import SignUp from "./components/SignUp";
 import NavBar from "./components/NavBar";
 import setAuthToken from "./utils/authentication";
